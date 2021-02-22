8bd1adb925ec60c0a8452acf03647a152c55db10
gothician <gothicianalter@gmail.com>
2021-02-22 16:47:52 +0200

    localStorage all done

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -14,27 +14,33 @@ export default class App extends Component {
   todoQtty = 0;
 
   state = {
-    todoData: this.localStorageService.getTodoData(),
+    todoData: [],
     filter: 'all',
     search: ''
   }
 
   componentDidMount() {
+    //Get data from localStorage
     const todoData = this.localStorageService.getTodoData();
+    // Set initial state
     this.setState({
       todoData
     });
+    // Set data length
     this.todoQtty = todoData.length;
   }
 
   updateTodoDataState = (todoData) => {
+    // Put data to localStorage
     this.localStorageService.putTodoData(todoData)
+    // Update state from localStorage
     this.setState({
       todoData: this.localStorageService.getTodoData()
     });
   }
 
   createTodoItem(label) {
+    // Set todo proterties and increment actual data length
     return {
       id: this.todoQtty++ + 1,
       label: label,
@@ -44,45 +50,42 @@ export default class App extends Component {
   }
 
   addItem = (label) => {
+    // Call update state function with new data
     this.updateTodoDataState({
       todoData: [...this.state.todoData, this.createTodoItem(label)]
-    })
+    });
   }
 
   deleteItem = (id) => {
-    this.setState(({ todoData }) => {
-      this.todoQtty--;
-      return {
-        //return array of todo elements with ids is not equal deleted id
-        todoData: todoData.filter((el) => el.id !== id)
-      }
-    })
+    // Decrement actual data length
+    this.todoQtty--;
+    //return array of todo elements with ids is not equal deleted id
+    const todoData = this.state.todoData.filter((el) => el.id !== id);
+    // Call update state function with new data
+    this.updateTodoDataState({ todoData });
 
   }
 
   toggleProperty = (arr, id, propName) => {
+    // Return todos attay with toggled property
     return arr.map((el) => {
-      // if id is equal, toggle property
       if (el.id === id) el.[propName] = !el.[propName];
-      // return todo element
       return el;
     })
   }
 
   onToggleDone = (id) => {
-    this.setState(({ todoData }) => {
-      return {
-        todoData: this.toggleProperty(todoData, id, 'done')
-      }
-    })
+    // Call function to toggle property
+    const todoData = this.toggleProperty(this.state.todoData, id, 'done');
+    // Update state
+    this.updateTodoDataState({ todoData });
   }
 
   onToggleImportant = (id) => {
-    this.setState(({ todoData }) => {
-      return {
-        todoData: this.toggleProperty(todoData, id, 'important')
-      }
-    })
+    // Call function to toggle property
+    const todoData = this.toggleProperty(this.state.todoData, id, 'important');
+    // Update state
+    this.updateTodoDataState({ todoData });
   }
 
   onFilterSelected = (id) => {
@@ -90,16 +93,10 @@ export default class App extends Component {
   }
 
   statusFilter = (data) => {
-
+    // Make choose, what data will be displayed (all, done or not done)
     if (this.state.filter !== 'all') return (this.state.filter === 'done')
       ? data.filter((el) => el.done)
       : data.filter((el) => !el.done);
-
-    /*switch (this.state.filter) {
-      case 'done': return = data.filter((el) => el.done);
-      case 'active': return data.filter((el) => !el.done);
-      default: return data;
-    }*/
 
     return data
   }
--- src/services/local-storage-service.js
+++ src/services/local-storage-service.js
@@ -1,19 +1,17 @@
 export default class LocalStorageService {
 
     getTodoData = () => {
+        // Get data from localStorage
         const item = localStorage.getItem('todoData');
-
+        // Parse JSON
         console.log(JSON.parse(item));
+        // If there were data - return them, else - return void array
         if (item) return JSON.parse(item).todoData;
         return [];
-        // return [
-        //     { id: 1, label: 'Drink Coffee', important: false, done: false },
-        //     { id: 2, label: 'Learn React', important: true, done: false },
-        //     { id: 3, label: 'Make Awesome App', important: false, done: false }
-        // ]
     }
 
     putTodoData = (todoData) => {
+        // Put data into localStorage
         localStorage.setItem('todoData', JSON.stringify(todoData));
     }
 }
